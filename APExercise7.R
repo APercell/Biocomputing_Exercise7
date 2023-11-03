63ccf337186abd167722fde3a32d242f022b5c55
APercell <146005859+APercell@users.noreply.github.com>
2023-11-03 05:52:41 -0400

    Updated APExercise7.R

--- APExercise7.R
+++ APExercise7.R
@@ -1,9 +1,9 @@
 # To import this data into a df run this code 
-> wagesdf <- read.delim2("~/Downloads/Lecture18/wages.csv")
+wagesdf <- read.delim2("~/Downloads/Lecture18/wages.csv")
 # To view
-> wagesdf
+wagesdf
 # To save as tab delimited file
-> write.table(wagesdf,
+write.table(wagesdf,
             file = "~/Downloads/Lecture18/iris.txt",
             row.names = F, sep = "/t")
 
@@ -12,25 +12,25 @@
 # To make list, first make each item in list
 
 # To make vector with length 10 containing 100,200,..100
-> vec2.1<-c(100,200,300,400,500,600,700,800,900,1000)
+vec2.1<-c(100,200,300,400,500,600,700,800,900,1000)
 # To make df with game stats
-> teamnames=c("Notre Dame Fighting Irish","Pittsburgh Panthers")
-> finalscore=c(58,7)
-> df2.2=data.frame(teamnames,finalscore,stringsAsFactors = FALSE)
-> df2.2
+teamnames=c("Notre Dame Fighting Irish","Pittsburgh Panthers")
+finalscore=c(58,7)
+df2.2=data.frame(teamnames,finalscore,stringsAsFactors = FALSE)
+df2.2
 # To add just the number 999 to list
-> number=999
+number=999
 # To make 10-row, 5 col matrix 
-> mat2.4<-matrix(1:50,nrow = 10,ncol = 5)
+mat2.4<-matrix(1:50,nrow = 10,ncol = 5)
 # To make vector with 3 letters
-> vec2.5<-c("a","b","c")
+vec2.5<-c("a","b","c")
 
 #Now that the parts are made, make list
-> listE7=list(
+listE7=list(
   + vec2.1, 
   + df2.2,
   + number=999,
   + mat2.4,
   + vec2.5)
 # To view list
-> listE7
+listE7
